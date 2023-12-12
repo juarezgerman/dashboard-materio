08290f2e5f0062c502f30b559556217e3b651dd2
juarezgerman <juarezgerman75@gmail.com>
2023-12-12 13:07:24 -0300

    elimine banner upgrade

--- typescript-version/src/App.vue
+++ typescript-version/src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import UpgradeToPro from '@/components/UpgradeToPro.vue';
+//import UpgradeToPro from '@/components/UpgradeToPro.vue';
 </script>
 
 <template>
   <VApp>
     <RouterView />
-    <UpgradeToPro />
+    <!--<UpgradeToPro />-->
   </VApp>
 </template>
